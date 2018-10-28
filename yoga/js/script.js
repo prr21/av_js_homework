269873c2283ef04392e3cc130ae51fa9040ac1c6
Kurban <mr.gustav009@gmail.com>
2018-10-28 23:14:23 +0300

    Изменение директорий

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// Переключение табов
 var info_header = document.getElementsByClassName('info-header')[0],
 	info_header_tab = document.getElementsByClassName('info-header-tab'),
 	tab = document.getElementsByClassName('info-tabcontent');
@@ -19,52 +20,54 @@ info_header.addEventListener('click', function(event) {
 	}
 });
 
-	var deadline = '8-09-2018';			
 
-	function getTime(endtime) {				
-		let t = Date.parse(endtime) - Date.parse(new Date),
-			hours = Math.floor(t/60/60/1000),
-			minutes = Math.floor(t/60/1000 % 60),
-			seconds = Math.floor(t/1000 % 60);
+// Обратный отсчет
+var deadline = '8-09-2018';			
 
-			if (t <= 0) {
-				hours = '00';
-				minutes = '00';
-				seconds = '00';
-			}
+function getTime(endtime) {				
+	let t = Date.parse(endtime) - Date.parse(new Date),
+		hours = Math.floor(t/60/60/1000),
+		minutes = Math.floor(t/60/1000 % 60),
+		seconds = Math.floor(t/1000 % 60);
 
-		return {
-			'total': t,
-			'hours': hours,
-			'minutes': minutes,
-			'seconds': seconds
+		if (t <= 0) {
+			hours = '00';
+			minutes = '00';
+			seconds = '00';
+		}
+
+	return {
+		'total': t,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	}
+}
+	
+function setTimer(id,endtime) {
+
+	let	timer = document.getElementById(id),			
+		hours = timer.querySelector('.hours'),	
+		minutes = timer.querySelector('.minutes'),
+		seconds = timer.querySelector('.seconds'),
+		timeInterval = setInterval(updateTime, 1000);
+
+	function updateTime() {
+		let t = getTime(endtime);
+		hours.innerHTML = t.hours;
+		minutes.innerHTML = t.minutes;
+		seconds.innerHTML = t.seconds;
+
+		if (t.total <= 0) {
+			clearInterval(timeInterval);
 		}
 	}
-		
-	function setTimer(id,endtime) {
-
-		let	timer = document.getElementById(id),			
-			hours = timer.querySelector('.hours'),	
-			minutes = timer.querySelector('.minutes'),
-			seconds = timer.querySelector('.seconds'),
-			timeInterval = setInterval(updateTime, 1000);
-
-		function updateTime() {
-			let t = getTime(endtime);
-			hours.innerHTML = t.hours;
-			minutes.innerHTML = t.minutes;
-			seconds.innerHTML = t.seconds;
-
-			if (t.total <= 0) {
-				clearInterval(timeInterval);
-			}
-		}
-		updateTime();
-	}
+	updateTime();
+}
 
 setTimer('timer', deadline);
 
-
+// Незаконченный скрипт с плавным скроллом
 /*	let ul = document.getElementsByTagName('ul')[0];
 
 	ul.addEventListener('click', function(event){
@@ -74,6 +77,9 @@ setTimer('timer', deadline);
 		}
 	})*/
 
+
+
+// Использовние конструктора и медота call
 let age = document.getElementById('age');
 
 function showUser(surname, name) {
@@ -92,6 +98,8 @@ function MakeNewUser(surname, name, value) {
 }
 
 
+
+// Всплытие и закрытие модальных окон
 let descrBtn = document.querySelectorAll('.description-btn'),
 	info = document.getElementsByClassName('info')[0],
 	overlay = document.getElementsByClassName('overlay')[0],
@@ -112,6 +120,8 @@ info.addEventListener('click', function(e) {
 	}
 });
 
+
+// Сделать div с заданными параметрами при помощи class и конструктора
 class Options {
 
 	constructor(height = 'auto', width = 'auto', bg = 'white', fontSize = 'inherit', textAlign = 'left'){
@@ -123,7 +133,7 @@ class Options {
 	};
 
 	makeDiv(text,block){
-		this.block = document.getElementById(block) || document.body;
+		this.block = $(block) || document.body;
 		this.div = document.createElement('div');
 
 		this.block.appendChild(this.div);
@@ -145,3 +155,35 @@ let text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiu
 
 let prr = new Options('200px', '400px', '#e8e8e8', '26px', 'right');
 // prr.makeDiv(text);
+
+
+// Ajax запрос
+var	form = document.getElementsByClassName('main-form')[0],
+	message = {
+		loading: "loading",
+		success: 'everything is fine',
+		fail: 'something is wrong'
+	},
+	div = document.createElement('div');
+
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+	var xhr = new XMLHttpRequest();
+
+	xhr.open('POST', 'server.php');
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	let formData = new FormData(form);
+
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState != 4){
+			div.innerHTML = message.loading;
+			form.appendChild(div);
+		} else if(xhr.readyState === 4){
+			if (xhr.status == 200 && xhr.status < 300){		
+				div.innerHTML = message.success;
+				form.appendChild(div);
+			}
+		} else form.appendChild(div.innerHTML = message.fail);
+	}
+	xhr.send(formData)
+});
