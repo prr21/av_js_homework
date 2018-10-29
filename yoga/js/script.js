fc88ce5d5ea2a1c9060ecf3514c6253a2f913010
prr <Mr.gustav009@gmail.com>
2018-10-29 23:41:52 +0300

    Написан калькулятор

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -187,3 +187,28 @@ form.addEventListener('submit', function(event) {
 	}
 	xhr.send(formData)
 });
+
+// Calculator
+var peopleSum = document.getElementsByClassName('counter-block-input')[0],
+	daySum = document.getElementsByClassName('counter-block-input')[1],
+	total = document.getElementById('total'),
+	peopleValue = 0,
+	dayValue = 0;
+
+	peopleSum.addEventListener('change', function() {
+		peopleValue = +this.value;
+		if(dayValue) caclPrice();
+	});
+
+	daySum.addEventListener('change', function() {
+		dayValue = +this.value;
+		if(peopleValue) caclPrice();
+	});
+
+	select.addEventListener('change', function() {
+		caclPrice( this.options[this.selectedIndex].value )
+	})
+
+	function caclPrice(place = 1) {
+		total.innerHTML = (dayValue + peopleValue) * 4000 * place;
+	}
