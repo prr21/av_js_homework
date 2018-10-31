06da1690f7d8f5631174f250b099021d17968733
prr <Mr.gustav009@gmail.com>
2018-10-31 03:33:44 +0300

    Дописан слайдер

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -218,41 +218,42 @@ var prev = document.getElementsByClassName('prev')[0],
 	next = document.getElementsByClassName('next')[0],
 	dotsWrap = document.getElementsByClassName('slider-dots')[0],
 	dots = document.getElementsByClassName('dot'),
-	sliders = document.getElementsByClassName('slider-item');
+	sliders = document.getElementsByClassName('slider-item'),
+	curSlide = 0;
 
 prev.addEventListener('click', function() {
-	slideUp(-1)
+	slideOn(curSlide--)
 });
 
 next.addEventListener('click', function() {
-	slideUp(+1)
+	slideOn(curSlide++)
 });
 
-// dotsWrap.addEventListener('click', function(event) {
-// 	if (event.target.classList.contains('dot')) {
+dotsWrap.addEventListener('click', function(event) {
+	for (let i = 0; i < dots.length; i++) {
 
-// 		for (let i = 0; i < dots.length; i++) {
-// 			if (event.target == dots[i]) slideUp(i)
-// 			dots[i].classList.remove('dot-active')
-// 		}
-
-// 		event.target.classList.add('dot-active');
-// 	}
-// })
-
-function slideUp(index) {
-
-	for (let i = 0; i < sliders.length; i++) {
-		if(!sliders[i].classList.contains('hide')) {
-			var currSlide = i;
-
-			if (currSlide + index >= sliders.length) currSlide = 0;
-			if (currSlide + index <= 0) currSlide = sliders.length;
-
-			sliders[i].classList.add('hide');
-			sliders[currSlide + index].classList.remove('hide');
+		if (event.target.classList.contains('dot') && event.target == dots[i]) {
+			slideOn(curSlide =+ i);
 			break
 		}
+
 	}
+});
+
+function slideOn() {
+
+	for (let i = 0; i < sliders.length; i++) {
+		dots[i].classList.remove('dot-active');
+		sliders[i].classList.add('hide');
+	}
+
+	if (curSlide <= -1) {
+		curSlide = sliders.length - 1;
+	} else if (curSlide >= sliders.length) {
+		curSlide = 0;
+	}
+	
+	dots[curSlide].classList.add('dot-active');
+	sliders[curSlide].classList.remove('hide');
 }
 
